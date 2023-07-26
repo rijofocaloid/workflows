f4be5e05a03d904e71b3a34f2b134a8da1956b89
rijofocaloid <137074586+rijofocaloid@users.noreply.github.com>
2023-07-26 15:16:52 +0530

    Update owasp_scan.sh

--- .github/workflows/owasp_scan.sh
+++ .github/workflows/owasp_scan.sh
@@ -3,6 +3,6 @@
 # Perform the OWASP ZAP scan and generate the HTML report
 zap-cli --quick-scan --spider https://dev-bo.nephroplus.com/
 zap-cli --active-scan https://dev-bo.nephroplus.com/
-zap-cli --report /tmp/scan-report.html -o html
+zap-cli --report /github/workspace/scan-report.html -o html
 
 echo "OWASP ZAP scan completed!"
