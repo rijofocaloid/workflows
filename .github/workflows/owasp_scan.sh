57c40bd864e911f97aedae721f1ad57e61370b64
rijofocaloid <137074586+rijofocaloid@users.noreply.github.com>
2023-07-26 15:47:40 +0530

    Update and rename owasp_scan.sh to scan.yaml

--- .github/workflows/owasp_scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Perform the OWASP ZAP scan and generate the HTML report
-zap-cli --quick-scan --spider https://dev-bo.nephroplus.com/
-zap-cli --active-scan https://dev-bo.nephroplus.com/
-zap-cli --report /github/workspace/scan-report.html -o html
-
-echo "OWASP ZAP scan completed!"
